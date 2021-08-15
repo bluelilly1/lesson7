d412e57f606809b7195b8989f1dbae603b1dac92
Bliss Singhal <54075580+bluelilly1@users.noreply.github.com>
2021-08-15 14:22:16 -0700

    Create lesson7.java

---
+++ lesson7.java
@@ -0,0 +1,18 @@
+//import your libraries here
+
+class Main {
+  //you will create a program that takes in the month number of that the user inputs into the console and prints out the month that it is
+  //Example: if the user inputs 2, the computer will print out "February"
+  
+  //create an array with the names of all the months of the calender as an attribute
+
+  //create a method that gets that month at that index
+
+  //create a method that converts the what the user inputs into an integer and returns that integer, which will be the index value
+  //remember that indexes for arrays start at zero so you will have to do some extra steps
+  
+  
+  public static void main(String[] args) {
+    //test out your program here
+  }
+}
